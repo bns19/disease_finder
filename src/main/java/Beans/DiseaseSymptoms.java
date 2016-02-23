6de4dbadd7ae6d2ea0445292101ff5bddff9cb0b
Bas1993 <bassss1993@gmail.com>
2016-02-23 10:17:44 +0100

    first commit

---
+++ src/main/java/Beans/DiseaseSymptoms.java
@@ -0,0 +1,16 @@
+package Beans;
+
+/**
+ * Created by bnsikkema on 23-2-16.
+ */
+public class DiseaseSymptoms {
+    private String[] symptomList;
+
+    public String[] getSymptomList() {
+        return symptomList;
+    }
+
+    public void setSymptomList(String[] symptomList) {
+        this.symptomList = symptomList;
+    }
+}
