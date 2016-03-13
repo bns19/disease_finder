26b895cbbdac4d421ef056623c8ba62e2a4f243e
henridupon <henridupon@gmail.com>
2016-03-13 14:34:33 +0100

    adjusted bug and comments in WebSecurityConfig

--- src/main/java/nl/bioinf/diseasefinderSpring/login/WebSecurityConfig.java
+++ src/main/java/nl/bioinf/diseasefinderSpring/login/WebSecurityConfig.java
@@ -1,26 +1,20 @@
 package nl.bioinf.diseasefinderSpring.login;
 
-import nl.bioinf.diseasefinderSpring.login.password.PasswordPass;
-import nl.bioinf.diseasefinderSpring.login.password.PasswordUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.validation.Valid;
-import javax.validation.constraints.AssertFalse;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import javax.sql.DataSource;
 
+
+/*
+@Configuration indicates that the class can be used by the Spring IoC container as a source of bean definitions.
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -29,11 +23,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-//                When adding websites to the antMatcher, these websites are not mandatory to be logged in (/** is all)
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -46,27 +40,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public String configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    DataSource dataSource;
 
-        String sql = "select username from User";
-        Object mysqlSelectUser = jdbcTemplate.queryForList(sql, new MapSqlParameterSource());
-        mysqlSelectUser = mysqlSelectUser.toString().replace("[","");
-        mysqlSelectUser = mysqlSelectUser.toString().replace("]","");
 
-        System.out.println(mysqlSelectUser.toString());
+    /*The @Autowired annotation provides more fine-grained control over where and how autowiring should be accomplished.
+    The @Autowired annotation can be used to autowire bean on the setter method just like @Required annotation, constructor,
+    a property or methods with arbitrary names and/or multiple arguments.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-        List<String> usernameList = Arrays.asList(mysqlSelectUser.toString().split(","));
-
-        for (String items : usernameList){
-            String AdjustedItem = PasswordUser.PasswordPass(items);
-            System.out.println(AdjustedItem);
-
-        }
+        auth.
+                jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from User where username = ?")
+                .authoritiesByUsernameQuery("select username, authority from User where username = ?");
 
 //        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
 //        auth.inMemoryAuthentication().withUser("henri").password("12345").roles("USER");
-        return "redirect:/results";
+
     }
-
-
 }
+
+
