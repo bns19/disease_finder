75a753d17f1e9c3b4d13fc1a59137017b4e85429
Bas1993 <bassss1993@gmail.com>
2016-03-22 12:32:18 +0100

    Tabs and save function works.

---
+++ src/main/java/nl/bioinf/diseasefinderSpring/HPOProcessor/HPOJsonObjectCreator.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.bioinf.diseasefinderSpring.HPOProcessor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ *
+ * @author aroeters
+ */
+public class HPOJsonObjectCreator {
+
+    /**
+     * Contains the hpoTerms.
+     */
+    private final HashMap<String, HPOTerm> hpoCollection;
+
+
+    /**
+     * The constructor of the class.
+     *
+     * @throws IOException when the file is not found
+     */
+    public HPOJsonObjectCreator() throws IOException {
+        String path = HPOJsonObjectCreator.class.getClassLoader()
+                .getResource(File.separator + "config" + File.separator
+                        + "hp.obo").toString();
+        HPOFileReader hr = new HPOFileReader(path.split(":")[1]);
+        this.hpoCollection = hr.readFile().getHPOHashMap();
+
+    }
+
+    /**
+     * Creates the child branch of the requested parent.
+     *
+     * @param hpoTerm the HPoTerm object of the parent
+     * @param parent the id of the parent
+     * @return a HashMap with all the information for jsTree.js
+     */
+    public final HashMap createSubTree(final HPOTerm hpoTerm,
+            final String parent) {
+        HashMap tree = new HashMap();
+        HashMap<String, Boolean> state = new HashMap<String, Boolean>();
+        state.put("opened", false);
+        state.put("selected", false);
+        tree.put("id", hpoTerm.getId());
+        tree.put("text", hpoTerm.getName());
+        tree.put("parent", parent);
+        tree.put("state", state);
+        if (hpoTerm.hasChildren()) {
+            tree.put("children", true);
+        } else {
+            tree.put("children", false);
+        }
+        return tree;
+    }
+}
---
+++ src/main/java/nl/bioinf/diseasefinderSpring/HPOProcessor/HPOTerm.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.diseasefinderSpring.HPOProcessor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author aroeters
+ */
+public class HPOTerm {
+    /**
+     * Contains the id op the HPO term.
+     */
+    private String id;
+    /**
+     * Contains the name of the HPO term.
+     */
+    private String name;
+    /**
+     * Contains the definition of the HPO term.
+     */
+    private String def;
+    /**
+     * isA list contains the is_a relations ships between the HPO terms.
+     */
+    private final List<String> isARelations = new ArrayList<String>();
+    /**
+     * Contains all the children of the HPOTerm.
+     */
+    private HashMap<String, HPOTerm> children;
+    /**
+     * Checks if the HPOTerm has children.
+     * @return True if the HPOTerm has children
+     */
+    public final boolean hasChildren() {
+        return children != null && !children.isEmpty();
+    }
+    /**
+     * Adds children to the HPOTerm.
+     * @param hpoTerm the term that has to be added.
+     */
+    public final void addChild(final HPOTerm hpoTerm) {
+        if (children == null) {
+            children = new HashMap<String, HPOTerm>();
+        }
+        if (children.containsKey(hpoTerm.getId())) {
+            throw new IllegalArgumentException("child already "
+                    + "exists on this node: " + hpoTerm);
+        }
+        children.put(hpoTerm.getId(), hpoTerm);
+    }
+    /**
+     * Gets a single child out of the HPOTerm.
+     * @param childId the id of the child
+     * @return the child with the given id
+     */
+    public final HPOTerm getChild(final String childId) {
+        return children.get(childId);
+    }
+    /**
+     * Returns all the children of the HPOTerm.
+     * @return all the children in a List
+     */
+    public final ArrayList<HPOTerm> getChildren() {
+        ArrayList<HPOTerm> terms = new ArrayList<HPOTerm>();
+        if (this.children == null) {
+            terms.addAll(Collections.EMPTY_LIST);
+        } else {
+            terms.addAll(this.children.values());
+        }
+        return terms;
+    }
+    /**
+     * The getter of the id.
+     * @return id of the HPO term
+     */
+    public final String getId() {
+        return id;
+    }
+    /**
+     * The setter of the id.
+     * @param newId the id of the HPOTerm
+     */
+    public final void setId(final String newId) {
+        if (newId == null) {
+            throw new NullPointerException("No id is given.");
+        }
+        this.id = newId;
+    }
+    /**
+     * The getter of the name.
+     * @return name of the HPO term
+     */
+    public final String getName() {
+        return name;
+    }
+    /**
+     * The setter of the name.
+     * @param setName String containing the name of the HPO term
+     */
+    public final void setName(final String setName) {
+        if (setName ==  null) {
+            throw new NullPointerException("No name is given.");
+        }
+        this.name = setName;
+    }
+    /**
+     * The getter of the definition.
+     * @return definition of the HPO term
+     */
+    public final String getDef() {
+        return def;
+    }
+    /**
+     * The setter of the Definition.
+     * @param setDef String containing the definition
+     */
+    public final void setDef(final String setDef) {
+        if (setDef == null) {
+            throw new NullPointerException("No definition is given.");
+        }
+        this.def = setDef;
+    }
+    /**
+     * returns all the isA relations in a list containing Strings.
+     * @return isA relations in a list
+     */
+    public final List getIsA() {
+        return isARelations;
+    }
+    /**
+     * Should get a String with a is a relation.
+     * @param addIsARelation should be a String with the isA relation
+     */
+    public final void addIsA(final String addIsARelation) {
+        if (addIsARelation == null) {
+            throw new NullPointerException("No relation given");
+        }
+        isARelations.add(addIsARelation);
+    }
+}
