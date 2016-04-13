76425fc27ef5a29f988a6c8910cc83d65e4acefb
Bas1993 <bassss1993@gmail.com>
2016-04-13 12:18:46 +0200

    organized

--- src/main/resources/static/js/googleTranslate.js
+++
@@ -1,5 +0,0 @@
-function translateInput(){
-    for(i=0; i<$('input').length; i++){
-        $('input').eq(i).val($('value').eq(i).text());
-    }
-}
--- src/main/resources/templates/tables.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS History(id SERIAL, searchedsymptoms VARCHAR(255), username VARCHAR(255));
-
-
-
