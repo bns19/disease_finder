6689929aa7e0cfb06cf9a27e083ff2b89974616b
hjdupon <henridupon@gmaoil.com>
2016-05-09 15:56:57 +0200

    dummy data toegevoegd

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (username, password, confirm_password, email, enabled, authority, created_at) VALUES('krishna', '21a4ed0a0cf607e77e93bf7604e2bb1ad07757c5', '21a4ed0a0cf607e77e93bf7604e2bb1ad07757c5', 'krishna@hotmail.com', 'true', 'USER','2016-05-04 14:48:54');
+INSERT INTO user (username, password, confirm_password, email, enabled, authority, created_at) VALUES('henri', 'dsdas123123132131231321ad', 'dsdas123123132131231321ad', 'henri@hotmail.com', 'true', 'USER','2016-09-04 14:54:54');
--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO 'user' ('username', 'password', 'confirm_password', 'email', 'enabled', 'authority', 'created_at') VALUES
-	('krishna', '21a4ed0a0cf607e77e93bf7604e2bb1ad07757c5', '21a4ed0a0cf607e77e93bf7604e2bb1ad07757c5', 'krishna@hotmail.com', 'true', 'USER','2016-05-04 14:48:54' );
