52afe85a739e879383ab1b84753b1412b6dfe732
henridupon <henridupon@gmail.com>
2016-06-21 23:43:29 +0200

    wachtwoord doet het bijna goed. Alleen je moet nadat je de wachtwoorden hebt ingetypt nog ergens anders naast de tekstvelden klikken

--- src/main/resources/static/js/controlemodal.js
+++ src/main/resources/static/js/controlemodal.js
@@ -1,98 +1,46 @@
-function validateLoginForm() {
-}
 
-function validateRegisterForm() {
+function checkPasswords() {
+    var password = document.forms["registerform"]["password"].value;
+    var confirmPassword = document.forms["registerform"]["confirmPassword"].value;
 
-    console.log("eerste")
-//
-//    var username = document.forms["registerform"]["username"].value;
-//    var password = document.forms["registerform"]["password"].value;
-//    var confirmPassword = document.forms["registerform"]["confirmPassword"].value;
-//    var email = document.forms["registerform"]["email"].value;
-//
-//    var booleanCheckPasswords = checkPasswords(confirmPassword, password)
-//    var booleanCheckifNull = checkifNull(confirmPassword, password)
-//    console.log("ervoor")
-//    //getRegistered(username);
-//
-//
-//
-//    if (booleanCheckPasswords == false) {
-//        return false;
-//    }
-//    if (booleanCheckifNull == false) {
-//        return false;
-//    }
-//    //if (booleanRegistered == false){
-//    //    return false;
-//    //}
-//    else {
-//        return true;
-//    }
-//
-//}
-//
-//
-//function checkifNull() {
-//
-//    if (username == null || username == "") {
-//        alert("username must be filled out");
-//        return false;
-//    }
-//    if (password == null || password == "") {
-//        alert("password must be filled out");
-//        return false;
-//    }
-//    if (confirmPassword == null || confirmPassword == "") {
-//        alert("confirm password must be filled out");
-//        return false;
-//    }
-//    if (email == null || email == "") {
-//        alert("email must be filled out");
-//        return false;
-//    }
-//    else {
-//        return true;
-//    }
-//
-//}
-//
-//
-//function checkPasswords(confirmPassword, password) {
-//    if (password != confirmPassword) {
-//        alert("passwords do not match");
-//        $('form[name=registerform]').submit(function (event) {
-//            event.preventDefault();
-//            //add stuff here
-//        });
-//        return false;
-//    }
-//    if (password == confirmPassword) {
-//
-//        return true;
-//    }
-//}
-//
-//function process(userdata) {
-//    console.log(userdata)
-//    if (userdata == "False") {
-//        booleanRegistered = false;
-//    }
-//    else {
-//        alert("Username already in use");
-//        booleanRegistered = true;
-//    }
+    document.getElementById("registerform").disabled = true;
 
+        if (password != confirmPassword) {
+            document.getElementById('password').style.borderColor = "red";
+            document.getElementById('confirmPassword').style.borderColor = "red";
+            document.getElementById("registerform").disabled = true;
+
+        }
+        if (password == confirmPassword) {
+            document.getElementById('password').style.borderColor = "green";
+            document.getElementById('confirmPassword').style.borderColor = "green";
+            document.getElementById("registerform").disabled = false;
+        }
 }
 
 
-//function getRegistered(username) {
-//    var url = "/getRegisteredUsers";
-//    $.get(url, {"username": username}, function (userdata) {
-//        alert("SSS" + userdata)
-//        process(userdata);
-//    });
-//}
+function process(userdata) {
+    //console.log(userdata)
+    //if (userdata == "False") {
+    //    booleanRegistered = false;
+    //}
+    //else {
+    //    alert("Username already in use");
+    //    booleanRegistered = true;
+    //}
+}
+
+
+function getRegistered() {
+
+    var username = document.forms["registerform"]["username"].value;
+    console.log(username)
+    var url = "/getRegisteredUsers";
+    $.get(url, {"username": username}, function (userdata) {
+        console.log(userdata)
+        process(userdata);
+    });
+}
 
 
 
